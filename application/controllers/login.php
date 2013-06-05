1cd8074a8f86eedfb669a1db59f145d390ada100
fatimaPHP <fatima.aurelia@klab.com>
2013-06-05 13:37:24 +0800

    Fix login redirection

---
+++ application/controllers/login.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+	public function index()
+	{
+        if ($this->session->userdata('auth') === true) {
+            redirect(base_url() . 'main');
+        }
+
+        $post = $this->input->post();
+
+        if(!$post) {
+            $this->load->view('login/index');
+        } else {
+            $user = $this->users->check_login($post['email'],$post['password']);
+
+            if ($user) {
+                $auth = array(
+                    'userid'       => $user->id,
+                    'username' => $user->name,
+                    'auth'         => true,
+                );
+
+                $this->session->set_userdata($auth);
+            }
+        }
+	}
+
+    public function logout()
+    {
+        $auth = array('userid' => '','username' => '','auth' => '');
+        $this->session->unset_userdata($auth);
+        redirect(base_url() . 'login');
+    }
+}
---
+++ application/views/login/index.php
@@ -0,0 +1,5 @@
+<form action="<?php echo base_url() . 'login' ?>" method="post">
+    <input type="text" name="email" placeholder="email" />
+    <input type="password" name="password" placeholder="password" />
+    <input type="submit" value="login">
+</form>
