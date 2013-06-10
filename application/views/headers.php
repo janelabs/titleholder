d5242d854909200005a94317491ac38b1ae4147f
Sarah Aristain <sarah@klab.com>
2013-06-10 15:43:58 +0800

    TH-10 | Connect arena module in main menu module

--- application/views/headers.php
+++ application/views/headers.php
@@ -11,7 +11,21 @@
     <script type="text/javascript" src="<?php echo base_url('assets/scripts/rpgJs/rpg-beta-2.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/scripts/bootstrap/js/bootstrap.min.js'); ?>"></script>
 
+    <script type="text/javascript">
+        $(function(){
+            $('#back_to_main').on("click", function(){
+                $("#arena_frame").fadeOut("slow",function(){
+                    $("#div_main").fadeIn("slow");
+                });
+            });
+        });
+    </script>
+
 </head>
 
 <body style="margin: 0; background-color: #fff;"> <!--  overflow: hidden; -->
-<noscript>You must enable your javascript to view this game!</noscript>
+<noscript>You must enable your javascript to view this game!</noscript><center>
+<div id="arena_frame" style="display: none;">
+    <iframe class="frame"></iframe>
+    <button id="back_to_main">MENU</button>
+</div>
